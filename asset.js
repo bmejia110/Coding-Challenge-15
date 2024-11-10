be0b618a0851409de2026d98088e606d48420bc2
Brandon Mejia <brandonmejiabusiness@gmail.com>
2024-11-10 15:22:45 -0500

    edits

--- asset.js
+++ asset.js
@@ -1,9 +1,9 @@
 //Task 1: Create the Asset Module
 
 export const assets = [
-    {id: 1, name: "AAPL", type: stock, price: 226, quantity: 10 }
-    {id: 2, name: "TSLA", type: stock, price: 321, quantity: 5 }
-    {id: 3, name: "NVDA", type: stock, price: 147, quantity: 10 }
+    {id: 1, name: "AAPL", type: stock, price: 226, quantity: 10 },
+    {id: 2, name: "TSLA", type: stock, price: 321, quantity: 5 },
+    {id: 3, name: "NVDA", type: stock, price: 147, quantity: 10 },
     {id: 4, name: "NFLX", type: stock, price: 795, quantity: 6 }
 
 ];
